a65513f9710cb610cfa6d234e94b92ae884c203d
KlausMass <massinemouha@zohomail.com>
2025-08-03 10:09:25 +0100

    feat: slowly decoupling from the SDL_Backend by depending solely on GL, maybe we can fully yonk it away eventually

--- include/core/graphics/texture.h
+++ include/core/graphics/texture.h
@@ -1,7 +1,13 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
 
+#include "glad/glad.h"
 #include <SDL3/SDL.h>
-typedef SDL_Texture Imzi_Texture;
+
+typedef struct {
+  int w; /**< The width of the texture, read-only. */
+  int h; /**< The height of the texture, read-only. */
+  GLuint id;
+} Imzi_Texture;
 
 #endif

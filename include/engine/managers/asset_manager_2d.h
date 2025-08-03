6bc0f389551e816442032dfd16397fd745f3fead
KlausMass <massinemouha@zohomail.com>
2025-08-03 10:11:36 +0100

    Since we no longer use SDL_LoadTexture, We can remove the dependency on the context where we had to pass the SDL_Renderer

--- include/engine/managers/asset_manager_2d.h
+++ include/engine/managers/asset_manager_2d.h
@@ -1,6 +1,7 @@
 #ifndef ASSET_MANAGER_2D_H
 #define ASSET_MANAGER_2D_H
 
+#include "core/imzi.h"
 #include "engine/visuals/animation.h"
 #include "engine/visuals/sprite.h"
 #include "engine/visuals/sprite_sheet.h"
@@ -16,6 +17,7 @@ typedef struct {
 
   Animation animations[MAX_ASSET_COUNT];
   int32_t animation_count;
+
 } AssetManager2D;
 
 // Rendering system will encapsulate the different assets we need and use them
@@ -23,19 +25,16 @@ typedef struct {
 
 void Imzi_SetupAssetManager2D(AssetManager2D *manager);
 
-int32_t Imzi_AssetManager2DLoadSpriteSheet(Imzi_Context_Ptr ctx,
-                                           AssetManager2D *manager,
+int32_t Imzi_AssetManager2DLoadSpriteSheet(AssetManager2D *manager,
                                            const char *path);
 
 int32_t Imzi_AssetManager2DGetSpriteSheet(AssetManager2D *manager,
                                           const char *path);
 
-int32_t Imzi_AssetManager2DGetOrLoadSpriteSheet(Imzi_Context_Ptr ctx,
-                                                AssetManager2D *manager,
+int32_t Imzi_AssetManager2DGetOrLoadSpriteSheet(AssetManager2D *manager,
                                                 const char *path);
 
-int32_t Imzi_AssetManager2DCreateSpriteFromPath(Imzi_Context_Ptr ctx,
-                                                AssetManager2D *manager,
+int32_t Imzi_AssetManager2DCreateSpriteFromPath(AssetManager2D *manager,
                                                 const char *path,
                                                 const char *name,
                                                 SDL_FRect *area);
@@ -47,9 +46,9 @@ int32_t Imzi_AssetManager2DGetSpriteByName(AssetManager2D *manager,
                                            const char *name);
 
 int32_t Imzi_AssetManager2DCreateAnimationFromPath(
-    Imzi_Context_Ptr ctx, AssetManager2D *manager, const char *path,
-    const char *name, SDL_FRect *area, float frame_h, float frame_w,
-    int32_t frame_count, double frame_time, bool loop);
+    AssetManager2D *manager, const char *path, const char *name,
+    SDL_FRect *area, float frame_h, float frame_w, int32_t frame_count,
+    double frame_time, bool loop);
 
 int32_t Imzi_AssetManager2DCreateAnimation(AssetManager2D *manager,
                                            int32_t sprite_sheet_index,
